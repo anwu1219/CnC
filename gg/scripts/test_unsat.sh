9070cd2e4ed4805dc86e79f25d4d0035acd37ae0
Alex Ozdemir <aozdemir@hmc.edu>
2020-03-24 15:42:30 -0700

    Move scripts agai

--- gg/scripts/test_unsat.sh
+++ gg/scripts/test_unsat.sh
@@ -3,10 +3,10 @@ numDivides=$2
 rm -rf test
 mkdir test
 cd test
-../splitter.sh ../../march_cu/march_cu ../$CNF $numDivides test.cnf
+../splitter.sh ../../../march_cu/march_cu ../$CNF $numDivides test.cnf
 for i in $(seq 0 $((2 **  $numDivides - 1)))
 do
-    ../solve.sh ../../iglucose/core/iglucose ../$CNF test.cnf.$i out.test.$i
+    ../solve.sh ../../../iglucose/core/iglucose ../$CNF test.cnf.$i out.test.$i
 done
 ../merge.sh out.test $numDivides merge-output.test
 if [[ $(cat merge-output.test) == "UNSAT" ]]
