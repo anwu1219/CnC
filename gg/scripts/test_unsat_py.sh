30502fc7a183effe36feed195178d5c87420b08d
Alex Ozdemir <aozdemir@hmc.edu>
2020-04-08 20:04:26 -0700

    Fix py test

--- gg/scripts/test_unsat_py.sh
+++ gg/scripts/test_unsat_py.sh
@@ -3,14 +3,15 @@ numDivides=$2
 rm -rf test
 mkdir test
 cd test
-../splitter.py ../../../march_cu/march_cu ../$CNF $numDivides test.cnf
+echo "a 0" > emptycnf
+../splitter.py ../../../march_cu/march_cu ../$CNF emptycnf $numDivides
 e=$((2 ** $numDivides - 1))
 for i in $(seq 0 $e)
 do
-    ../solve.py ../../../iglucose/core/iglucose ../$CNF test.cnf.$i out.test.$i
+    ../solve.py ../../../iglucose/core/iglucose ../$CNF cube$i sub$i
 done
-../merge.py $numDivides merge-output.test $(for i in $(seq 0 $e); do echo out.test.$i; done)
-if [[ $(cat merge-output.test) == "UNSAT" ]]
+../merge.py $numDivides $(for i in $(seq 0 $e); do echo sub$i; done)
+if [[ $(cat out) == "UNSAT" ]]
 then
     echo "UNSAT, as expected"
     rm -rf test
