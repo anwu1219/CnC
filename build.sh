c2bbc8240cba08100ddfa64e78e310cf69200291
Alex Ozdemir <aozdemir@hmc.edu>
2020-05-23 14:15:53 -0700

    add cadical

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-cd march_cu; make $1; cd ..;
-cd iglucose/core; make $1; cd ../simp; make $1; cd ../..;
-cd lingeling; ./configure.sh; make $1; cd ..;
+#!/usr/bin/env bash
+
+# Enable exit-on-error, and command log
+set -xe
+
+git submodule update --recursive
+(cd march_cu && make -j$(nproc))
+(cd iglucose/core && make -j$(nproc) rs && cd ../simp && make -j$(proc))
+(cd lingeling && ./configure.sh -static && make -j $(nproc))
+(cd cadical && env CXXFLAGS=-static ./configure && cd build && make -j $(nproc))
