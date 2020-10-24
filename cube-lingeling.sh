39430c364105b8720a6b3a6ce4803eaeffa139bc
anwu1219 <haozewu@stanford.edu>
2020-10-23 21:39:38 -0700

    clean up

--- cube-lingeling.sh
+++ cube-lingeling.sh
@@ -1,8 +1,28 @@
-DIR=/tmp
 CNF=$1
-./march_cu/march_cu $CNF -o $DIR/cubes$$ $2 $3 $4 $5 $6 $7 $8 $9
-echo "p inccnf" > $DIR/formula$$.icnf
-cat $CNF | grep -v c >> $DIR/formula$$.icnf
-cat $DIR/cubes$$ >> $DIR/formula$$.icnf
-time ./lingeling/ilingeling $DIR/formula$$.icnf -b 8
-rm $DIR/formula$$.icnf
+DIR=$2
+cuber=$3
+depth=$4
+
+if [[ $CNF == "--help" ]]; then
+    echo "usage: ./cube-lingeling.sh [CNF] [OUT_DIR] [march/louvain] [depth]"
+    exit
+fi
+
+if [[ -d $DIR ]]; then
+    echo "Directoy exists: $DIR"
+    exit
+fi
+
+mkdir $DIR
+
+if [[ $cuber == "march" ]]; then
+    ./march_cu/march_cu $CNF -o $DIR/cubes -d $depth
+else
+    ./march_cu/march_cu $CNF -o $DIR/cubes $5 $6 $7 $8 $9
+fi
+
+echo "p inccnf" > $DIR/formula.icnf
+cat $CNF | grep -v c >> $DIR/formula.icnf
+cat $DIR/cubes >> $DIR/formula.icnf
+time ./lingeling/ilingeling $DIR/formula.icnf -b 8
+rm $DIR/formula.icnf
