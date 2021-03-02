c5653f21616a1421774132cbaec323ace48f8531
Haoze Wu <haozewu@iam-ssh1.research.intel-research.net>
2021-03-02 14:34:07 -0800

    different preselect strategy

--- march_cu/preselect.c
+++ march_cu/preselect.c
@@ -264,15 +264,16 @@ void RealisePreselectedSet () {
 	if( max_preselected < 30 ) 	max_preselected = 30;
 */
 
-	if     (  depth <=  2  )    max_preselected = 400; else
-	if     (  depth <=  4  )    max_preselected = 200; else
+	if     (  depth <=  2  )    max_preselected = 10000; else
+	if     (  depth <=  4  )    max_preselected = 50000; else
 	if(    (  depth <=  8  )  ||
 	 (3 * nrofceq > freevars )  )    		  // multiplier
-				  max_preselected = 100;
-	else if( depth <= 12 )    max_preselected =  60;
-	else		          max_preselected =  30;
+				  max_preselected = 2500;
+	else if( depth <= 12 )    max_preselected =  1250;
+	else		          max_preselected =  625;
 
-//	max_preselected = (int) (percent * 0.01 * freevars );
+	//max_preselected = 10000;
+       //	max_preselected = (int) (percent * 0.01 * freevars );
 //	max_preselected = (int) (freevars / (1.0 + 0.3 * depth) );
 //	max_preselected = 200;
 	// max_preselected = freevars;
